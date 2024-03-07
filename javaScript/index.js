89b8ece1ac29ecd18cc73935d7138a0137d4d000
Mohsin860 <mohsinbhatti053@gmail.com>
2024-03-07 14:58:16 +0500

    pushing message

---
+++ javaScript/index.js
@@ -0,0 +1,12 @@
+//alert("Mohsin Bhatti");
+
+//console.log("Hii");
+
+//var email= 'mohsinbhatt053@gmail.com';
+
+//alert("mt email adress is " + email );
+
+var age= 2000;
+
+console.log('my birth year is' + age + ' and the data type is ' + typeof age);
+
